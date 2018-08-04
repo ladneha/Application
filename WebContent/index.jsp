21fcaf10cd99a29ae3ffcb44b6237d164a3ccc14
nehalad <nlad1@binghamton.edu>
2018-08-04 17:42:17 -0400

    login form

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,1 +1,5 @@
-<a href="hello.html">click</a>
+<form action="hello.html" method="post">
+Name: <input type = "text" name="name"/><br/>
+Password: <input type="password" name = "password"/><br/>
+<input type="submit" value="login"/>
+</form>
--- src/com/practice/HelloController.java
+++ src/com/practice/HelloController.java
@@ -1,5 +1,8 @@
 package com.practice;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,9 +11,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 	
 	@RequestMapping("/hello")
-	public ModelAndView helloWorld() {
-		String message = "Hello Spring MVC";
-		return new ModelAndView("hellopage", "message", message);
+	public ModelAndView helloWorld(HttpServletRequest request, HttpServletResponse response) {
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		
+		if(password.equals("admin")) {
+			String message = "Hello "+name;
+			return new ModelAndView("hellopage", "message", message);
+		}else {
+			return new ModelAndView("errorpage", "message", "Sorry username and password error");
+		}
 	}
 
 }
